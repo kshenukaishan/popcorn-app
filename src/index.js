53e82969b8aa75f65253211e41cefd547ee400fb
kshenukaishan <kshenukaishan@outlook.com>
2023-10-21 11:16:34 +0530

    implemented message to render and colors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={10} />
+    <StarRating
+      messages={["terrible", "bad", "okay", "good", "amazing"]}
+      color="red"
+    />
   </React.StrictMode>
 );
