597f1747a283b0de855f3727eebbc03c3f86e467
kshenukaishan <kshenukaishan@outlook.com>
2023-10-23 16:27:27 +0530

    added the api key and doesn't work

--- src/App.js
+++ src/App.js
@@ -50,9 +50,16 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const API_KEY = "62ad9bc4";
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=interstellar`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <>
       <Navbar>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
-// import "./index.css";
-// import App from "./App";
+// import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
+    {/* <StarRating
       messages={["terrible", "bad", "okay", "good", "amazing"]}
       color="red"
-    />
+    /> */}
+    <App />
   </React.StrictMode>
 );
